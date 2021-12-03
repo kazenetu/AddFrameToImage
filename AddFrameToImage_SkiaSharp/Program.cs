490dec40082ebcf2828b3d2af65c1eacae54e6b5
kazenetu <rakudafactory@gmail.com>
2021-12-03 22:19:01 +0900

    SkiaSharp版を追加 #2

---
+++ AddFrameToImage_SkiaSharp/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using SkiaSharp;
+using System.IO;
+
+namespace AddFrameToImage
+{
+    class Program
+    {
+        /// <summary>
+        /// エントリメソッド
+        /// </summary>
+        static void Main(string[] args)
+        {
+            if(args.Length < 1){
+                Console.WriteLine("addFrameImage ImagesDirectoryPath");
+                return;
+            }
+
+            Console.Write("running...");
+            
+            var directoryInfo = new DirectoryInfo(args[0]);
+            foreach(var fileInfo in directoryInfo.GetFiles()){
+                // イメージに枠をつける
+                addFrame(fileInfo.FullName);
+            }
+
+            Console.WriteLine("finish!");
+        }
+
+        /// <summary>
+        /// イメージに枠をつけて保存する
+        /// </summary>
+        /// <param name="filePath">イメージのパス</param>
+        static private void addFrame(string filePath)
+        {
+            using(var image =  SKImage.FromEncodedData(filePath))
+            using(var surface = SKSurface.Create(image.Info))
+            {
+                surface.Canvas.DrawImage(image,0,0);
+                surface.Canvas.DrawRegion(new SKRegion(new SKRectI(0,0,image.Width-1,image.Height-1)),new SKPaint{Color=SKColors.Black,Style = SKPaintStyle.Stroke});
+
+ 				// save the file
+				using (var saveImage = surface.Snapshot())
+                using(var data = saveImage.Encode())
+				using (var stream = File.OpenWrite(filePath))
+				{
+					data.SaveTo(stream);
+				}
+            }
+        }
+    }
+}
